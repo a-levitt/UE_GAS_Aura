823417e17ca2b9a4bd1fe4a5b32fd3b4a0c1a6dd
Artemis Levitt <artemis.levitt@gmail.com>
2024-04-10 11:07:40 +0200

    The base character class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// 2024 ALevitt
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// 2024 ALevitt
+
+#pragma once
+
+#include "CoreMinimal.h"
+
